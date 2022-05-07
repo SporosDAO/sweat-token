79e2b7992a08525d307f348deffeb36eea04ecdf
opny721 <opny721@gmail.com>
2022-05-07 15:20:11 +0200

    added initial tests

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,4 @@
 import * as dotenv from "dotenv";
-
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -7,6 +6,7 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import "hardhat-watcher";
 
 dotenv.config();
 
@@ -24,7 +24,28 @@ task("accounts", "Prints the list of accounts", async (taskArgs: unknown, hre: H
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  paths: {
+    artifacts: "./build/artifacts",
+    cache: "./build/cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  typechain: {
+    outDir: './build/types',
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
@@ -38,6 +59,13 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: "compile", params: { quiet: true } }, "test"],
+      files: ["./contracts", "./test"],
+      verbose: true,
+    },
   },
 };
 
---
+++ contracts/test/GigContractRoles.spec.ts
@@ -0,0 +1,40 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("GigContract", function () {
+
+  const setupContract = async (admin: string): Promise<Contract> => {
+    const GigContract = await ethers.getContractFactory("GigContract");
+    const gigContract = await GigContract.deploy(admin);
+    await gigContract.deployed();
+    return gigContract
+  }
+
+  it("Should enforce roles", async function () {
+
+    const [
+      owner,
+      pm1,
+      contrib1
+    ] = await ethers.getSigners();
+
+    const gigContract = await setupContract(await owner.getAddress())
+
+    // add pm1
+    await gigContract.addProjectManager(await pm1.getAddress())
+
+    // pm1 should be allowed to add contrib
+    expect(gigContract
+      .connect(pm1)
+      .addContributor(await contrib1.getAddress())
+    ).not.to.be.reverted
+
+    expect(gigContract
+      .connect(contrib1)
+      .releaseRewards(contrib1)
+    ).to.be.reverted
+
+    // expect(await gigContract.greet()).to.equal("Hello, world!");
+  });
+});
