7f8a6e2a39e8dbac11d3653f1360d3d60606ad32
opny721 <opny721@gmail.com>
2022-05-14 06:39:54 +0200

    added docker build

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,34 @@
-FROM node:lts
+FROM node:lts as deps
 
-WORKDIR /app
+WORKDIR /app/contracts
+ADD ./contracts/package.json ./
+RUN yarn install
 
+WORKDIR /app/backend
 ADD ./backend/package.json ./
 RUN yarn install
+
+WORKDIR /app/frontend
+ADD ./frontend/package.json ./
+RUN yarn install
+
+FROM deps as contracts
+
+WORKDIR /app/contracts
+ADD ./contracts/ .
+# RUN yarn build
+
+FROM contracts as backend
+
+WORKDIR /app/backend
+
 ADD ./backend/ .
 RUN yarn build
 
-CMD ["node", "dist/main"]
+FROM backend as frontend
+
+WORKDIR /app/frontend
+
+ADD ./frontend/ .
+RUN yarn build
+
--- backend/libs/runtime/src/runtime.module.ts
+++ backend/libs/runtime/src/runtime.module.ts
@@ -1,11 +1,22 @@
 import { DatabaseModule } from '@app/database'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ServeStaticModule, ServeStaticModuleOptions } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          rootPath: join(config.get('FRONTEND_BUILD_PATH')),
+        } as ServeStaticModuleOptions,
+      ],
     }),
     DatabaseModule,
   ],
