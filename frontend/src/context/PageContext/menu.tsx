ee4eb30df3c99fb5f9f3f71851ef7ee235e4b284
ivelin <ivelin.eth@gmail.com>
2022-05-25 01:30:55 +0000

    build: optimize docker build for dev
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- frontend/src/context/PageContext/menu.tsx
+++ frontend/src/context/PageContext/menu.tsx
@@ -1,5 +1,5 @@
 import { People } from '@mui/icons-material'
-import TaskIcon from '@mui/icons-material/Task'
+import FactoryIcon from '@mui/icons-material/Factory'
 import PieChartIcon from '@mui/icons-material/PieChart'
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong'
 import DashboardIcon from '@mui/icons-material/Dashboard'
@@ -44,7 +44,7 @@ export const menu: MenuItem[] = [
   },
   {
     label: 'Projects',
-    icon: TaskIcon,
+    icon: FactoryIcon,
     link: 'projects'
   },
   {
