845bcc220efb56bff8d2f6296fae5b07d7e889fa
ivelin <ivelin.eth@gmail.com>
2022-06-08 21:55:00 +0000

    chore: checkpoint
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- .gitpod.sh
+++ .gitpod.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# clean up any previously started and dangling containers
+docker-compose down
 # update install deps
 docker-compose -f docker-compose.yaml -f docker-compose.install.yaml --profile install up --no-deps
 # start app modules
@@ -9,4 +11,5 @@ export REACT_APP_SWEAT_TOKEN_API_BASEPATH=$(gp url 3001)
 docker-compose up -d frontend
 # change owner of files to gitpod
 # fixes problems when some files are installed as root by docker scripts
-sudo chown -hR gitpod.gitpod node_modules/
+sudo chown -hR gitpod.gitpod frontend/node_modules/
+sudo chown -hR gitpod.gitpod backend/node_modules/
