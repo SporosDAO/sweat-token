b2bb3f3606558b7edf3b591c9271a7afe5c032c5
ivelin <ivelin.eth@gmail.com>
2022-07-02 00:52:28 +0000

    chore: lint fix
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- frontend/src/components/MyDAOs.js
+++ frontend/src/components/MyDAOs.js
@@ -2,13 +2,13 @@ import { useNetwork, useAccount } from '../context/Web3Context'
 import { useGraph } from './hooks/useGraph'
 import { USER_DAOS } from '../graph'
 import { List, ListItem, Button, Card, CardContent, CardActions, Typography } from '@mui/material'
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query'
 import { request } from 'graphql-request'
 import { GRAPH_URL } from '../graph'
 
 export function useGetUserDAOs(chainId, userAddress) {
   return useQuery([chainId, userAddress, USER_DAOS], async () => {
-    const data = await request(GRAPH_URL[chainId], USER_DAOS, {address: userAddress})
+    const data = await request(GRAPH_URL[chainId], USER_DAOS, { address: userAddress })
     return data
   })
 }
@@ -16,22 +16,22 @@ export function useGetUserDAOs(chainId, userAddress) {
 export default function MyDAOs() {
   const { chain } = useNetwork()
   const { address, isConnecting, isDisconnected } = useAccount()
-  console.log({address, isConnecting, isDisconnected })
+  console.log({ address, isConnecting, isDisconnected })
 
-  const { data, error, isLoading, isSuccess } = useGetUserDAOs(chain?.id, address);
+  const { data, error, isLoading, isSuccess } = useGetUserDAOs(chain?.id, address)
 
   const daos = data?.['members']
 
-  console.log({chain})
-  console.log({daos})
+  console.log({ chain })
+  console.log({ daos })
 
   return (
     <>
-      {(isLoading || isConnecting || isDisconnected) ?
-        <div>Loading...</div> :
-        (daos && daos.length > 0) ?
-          <List>
-            {daos.map((dao) =>
+      {isLoading || isConnecting || isDisconnected ? (
+        <div>Loading...</div>
+      ) : daos && daos.length > 0 ? (
+        <List>
+          {daos.map((dao) => (
             <ListItem key={dao['dao']['id']}>
               <Card sx={{ minWidth: 275 }} raised={true}>
                 <CardContent>
@@ -49,15 +49,17 @@ export default function MyDAOs() {
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small" href="{`dao/chain/${chain.id}/address/${dao['dao']['id']}/dashboard`}">Details</Button>
+                  <Button size="small" href="{`dao/chain/${chain.id}/address/${dao['dao']['id']}/dashboard`}">
+                    Details
+                  </Button>
                 </CardActions>
               </Card>
             </ListItem>
-            )}
-          </List>
-          :
-          <div>You are not participating in any for-profit (Sporos-style) DAOs on chain: {chain.name}.</div>
-        }
+          ))}
+        </List>
+      ) : (
+        <div>You are not participating in any for-profit (Sporos-style) DAOs on chain: {chain.name}.</div>
+      )}
     </>
   )
 }
