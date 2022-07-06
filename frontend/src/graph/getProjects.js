2cdce77105b8f99f92b4e82f7cadead0cf63acfc
ivelin <ivelin.eth@gmail.com>
2022-07-06 00:36:07 +0000

    chore: checkpoint new projects page
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- frontend/src/graph/getProjects.js
+++ frontend/src/graph/getProjects.js
@@ -7,23 +7,60 @@ export const getProjects = async (chainId, daoAddress) => {
       method: 'POST',
       body: JSON.stringify({
         query: `query {
-            daos(where: {
-              id: "${daoAddress.toLowerCase()}"
+          projects(
+            orderBy: projectID,
+            orderDirection: desc,
+            where: {
+              dao {
+                address: "${daoAddress.toLowerCase()}"
+              }
             }) {
-                id
-                members {
-                    address
-                    shares
-                  }
-                  token {
-                    totalSupply
-                  }
+            active
+            budget
+            deadline
+            goals
+            id
+            manager
+            projectID
+            dao {
+              address
             }
-          }`
+          }
+        }`
       })
     })
     console.log('result', res)
-    const data = await res.json()
+    // uncomment when Kali subgraph restores access to projects
+    // const data = await res.json()
+    // in the meanwhile use mock data
+    const data = {
+      "projects": [
+        {
+          active: true,
+          budget: 5000,
+          deadline: 20220710,
+          goals: "New landing page",
+          id: "112312312312",
+          manager: "0xcc53685363e14914d28f2d37f226618451d4ef4c",
+          projectID: "100",
+          dao: {
+            address: "0x60c03712765c1211d02c460b93cfe12d2bbee1dc"
+          },
+        },
+        {
+          active: true,
+          budget: 10000,
+          deadline: 20220810,
+          goals: "New Project Management UI flow",
+          id: "1123123156742",
+          manager: "0xf952a72F39c5Fa22a443200AbE7835128bCb7439",
+          projectID: "101",
+          dao: {
+            address: "0x60c03712765c1211d02c460b93cfe12d2bbee1dc"
+          },
+        },
+      ]
+    }
     return data
   } catch (e) {
     return e
