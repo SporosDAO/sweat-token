153ce3ab9990838ad68138572c0ead8311442087
alex11ohm11 <102125623+alex11ohm11@users.noreply.github.com>
2022-08-10 08:31:57 +0200

    style: change default font to roboto

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -3,5 +3,19 @@ import { createTheme } from '@mui/material'
 export default createTheme({
   palette: {
     mode: 'dark'
+  },
+  typography: {
+    allVariants: {
+      fontFamily: ['Roboto'].join(','),
+      fontSize: 15
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: Roboto
+        }`
+    }
   }
 })
