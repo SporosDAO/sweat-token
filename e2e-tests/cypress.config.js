2b22a0517de14579fd9f0bde152cd86a298ad44f
ivelin <ivelin.eth@gmail.com>
2022-08-13 01:13:53 +0000

    feat: full e2e test for Sporos DAO with metamask
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  chromeWebSecurity: true
+  chromeWebSecurity: true,
 })
--- e2e-tests/tests/e2e/specs/home.js
+++ e2e-tests/tests/e2e/specs/home.js
@@ -1,12 +1,32 @@
-describe('Puppeteer', () => {
-  context('Sporos DAO App home page', () => {
-    it.only(`Should connect to home page`, () => {
-      cy.visit('/')
-      cy.contains('The Launchpad of For-Profit DAOs123')
-    })
-    it.only(`Should connect to Metamask`, () => {
+describe.only('Puppeteer', () => {
+  context.only('Sporos DAO App home page', () => {
+    it(`Should connect to home page`, () => {
       cy.visit('/')
       cy.contains('The Launchpad of For-Profit DAOs')
     })
+    it(`Should connect to Metamask`, () => {
+      cy.visit('/')
+      cy.contains('The Launchpad of For-Profit DAOs')
+    })
+    it.only(`Should show DAOs on Goerli for account 0xf952a72F39c5Fa22a443200AbE7835128bCb7439`, () => {
+      cy.visit('/')
+      cy.contains('Connect Wallet').click()
+      cy.contains('MetaMask').click()
+      cy.acceptMetamaskAccess().then(connected => {
+        expect(connected).to.be.true;
+      });
+      cy.allowMetamaskToSwitchNetwork()
+      cy.changeMetamaskNetwork('goerli').then(networkChanged => {
+        expect(networkChanged).to.be.true;
+      });
+      cy.switchMetamaskAccount('0xf952a72F39c5Fa22a443200AbE7835128bCb7439').then(accountChanged => {
+        expect(accountChanged).to.be.true;
+      });
+      cy.contains('Your DAOs')
+      cy.contains('PMTest')
+      cy.contains('Symbol: PMT')
+      cy.contains('Chain: Goerli')
+      cy.contains('Address: 0xe237747055b12f4da323bc559ac8d5eb66aac2f7')
+    });
   })
 })
