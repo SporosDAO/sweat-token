29d69362fd0ee3e6ff0d93c1919feb97c2a7cd6c
ivelin <ivelin.eth@gmail.com>
2022-08-17 19:49:11 +0000

    fix: e2e ci script
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- e2e-tests/tests/e2e/specs/projects.js
+++ e2e-tests/tests/e2e/specs/projects.js
@@ -1,12 +1,10 @@
 describe('Project Management', () => {
   context('Sporos DAO App Projects Pages', () => {
-    it(`Should connect to Metamask`, () => {
+
+    before(() => {
       cy.visit('/')
       cy.contains('Connect Wallet').click()
       cy.contains('MetaMask').click()
-      cy.acceptMetamaskAccess().then(connected => {
-        expect(connected).to.be.true;
-      });
     })
     it(`Should show DAO PMTest projects for account 0xf952a72F39c5Fa22a443200AbE7835128bCb7439`, () => {
       cy.visit('/')
