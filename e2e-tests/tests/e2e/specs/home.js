bc233eef75fe22015d326317450dd0df66935a6d
ivelin <ivelin.eth@gmail.com>
2022-08-17 14:46:24 +0000

    fix: e2e ci:
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- e2e-tests/tests/e2e/specs/home.js
+++ e2e-tests/tests/e2e/specs/home.js
@@ -6,15 +6,13 @@ describe('Puppeteer', () => {
     })
     it(`Should connect to Metamask`, () => {
       cy.visit('/')
-      cy.contains('The Launchpad of For-Profit DAOs')
-    })
-    it(`Should show DAOs on Goerli for account 0xf952a72F39c5Fa22a443200AbE7835128bCb7439`, () => {
-      cy.visit('/')
       cy.contains('Connect Wallet').click()
       cy.contains('MetaMask').click()
       cy.acceptMetamaskAccess().then(connected => {
         expect(connected).to.be.true;
       });
+    })
+    it(`Should show DAOs on Goerli for account 0xf952a72F39c5Fa22a443200AbE7835128bCb7439`, () => {
       cy.changeMetamaskNetwork('rinkeby').then(networkChanged => {
         expect(networkChanged).to.be.true;
       });
--- e2e-tests/tests/e2e/specs/projects.js
+++ e2e-tests/tests/e2e/specs/projects.js
@@ -1,34 +1,17 @@
-describe('Puppeteer', () => {
-  context('Sporos DAO App home page', () => {
-    it(`Should connect to home page`, () => {
-      cy.visit('/')
-      cy.contains('The Launchpad of For-Profit DAOs')
-    })
+describe('Project Management', () => {
+  context('Sporos DAO App Projects Pages', () => {
     it(`Should connect to Metamask`, () => {
-      cy.visit('/')
-      cy.contains('The Launchpad of For-Profit DAOs')
-    })
-    it(`Should show DAOs on Goerli for account 0xf952a72F39c5Fa22a443200AbE7835128bCb7439`, () => {
       cy.visit('/')
       cy.contains('Connect Wallet').click()
       cy.contains('MetaMask').click()
       cy.acceptMetamaskAccess().then(connected => {
         expect(connected).to.be.true;
       });
-      cy.changeMetamaskNetwork('rinkeby').then(networkChanged => {
-        expect(networkChanged).to.be.true;
-      });
-      cy.changeMetamaskNetwork('goerli').then(networkChanged => {
-        expect(networkChanged).to.be.true;
-      });
-      cy.switchMetamaskAccount('0xf952a72F39c5Fa22a443200AbE7835128bCb7439').then(accountChanged => {
-        expect(accountChanged).to.be.true;
-      });
-      cy.contains('Your DAOs')
-      cy.contains('PMTest')
-      cy.contains('Symbol: PMT')
-      cy.contains('Chain: Goerli')
-      cy.contains('Address: 0xe237747055b12f4da323bc559ac8d5eb66aac2f7')
+    })
+    it(`Should show DAO PMTest projects for account 0xf952a72F39c5Fa22a443200AbE7835128bCb7439`, () => {
+      cy.visit('/')
+      cy.contains('PMTest').click()
+      cy.contains('Projects')
     });
   })
 })
