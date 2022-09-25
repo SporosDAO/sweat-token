a17149c9ad184a97b3560657f22a89ce103ea946
keith33m <keith.33m@gmail.com>
2022-09-25 11:18:02 +0100

    add back old landing page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 
 import Dao from './pages/Dao'
 import People from './pages/People'
+import Landing from './pages/Landing'
 import DaoLanding from './pages/Dao/index'
 import CreateDao from './pages/Dao/create'
 import NotFound from './pages/NotFound'
@@ -15,7 +16,8 @@ const App = () => (
   <ThemeProvider theme={useTheme()}>
     <CssBaseline />
     <Routes>
-      <Route path="/" element={<DaoLanding />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/dao" element={<DaoLanding />} />
       {/* @todo - remove routes, only temporary for testing purposes */}
       <Route path="/dao/create" element={<CreateDao />} />
       <Route path="dao/chain/:chainId/address/:daoId" element={<Dao />}>
