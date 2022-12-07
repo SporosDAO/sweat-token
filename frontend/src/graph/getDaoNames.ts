633d1e1fe67aa549543987b7c988a1e8d87c4dcb
ivelin <ivelin.eth@gmail.com>
2022-12-07 20:47:59 +0000

    feat: validate new DAO name is unique
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

---
+++ frontend/src/graph/getDaoNames.ts
@@ -0,0 +1,31 @@
+import { useQuery } from 'react-query'
+import { GRAPH_URL } from './url'
+
+export async function getDaoNames(chainId: number) {
+  try {
+    const res = await fetch(GRAPH_URL[chainId], {
+      method: 'POST',
+      body: JSON.stringify({
+        query: `query {
+            daos(first: 1000) {
+              token {
+                name
+              }
+            }
+          }`
+      })
+    })
+    const data = await res.json()
+    const names = data?.data?.daos?.map((dao: any) => dao.token?.name)
+    return names
+  } catch (e) {
+    return e
+  }
+}
+
+export function useGetDaoNames(chainId: number) {
+  return useQuery(['getDaoNames', chainId], async () => {
+    const names = await getDaoNames(chainId)
+    return names
+  })
+}
--- frontend/src/pages/Dao/create/views/Name.tsx
+++ frontend/src/pages/Dao/create/views/Name.tsx
@@ -1,46 +1,62 @@
+import { ErrorMessage } from '@hookform/error-message'
+import { Alert } from '@mui/material'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import { useParams } from 'react-router'
+import { useGetDaoNames } from '../../../../graph/getDaoNames'
 
 import Header from '../../components/Header'
 
-const Name: React.FC<any> = (props) => (
-  <>
-    <Header title="Name" subtitle="Tell us the name of your company." />
-    <Box>
-      <TextField
-        fullWidth
-        type="text"
-        label="On-Chain name"
-        error={!!props?.formState?.errors.name}
-        placeholder="Pick a name"
-        {...props?.register('name', { required: true })}
-        helperText={
-          <>
-            {props?.formState?.errors.name && props?.formState?.errors.name.type === 'required'
-              ? 'You need to pick the company name in order to continue.'
-              : "This will be your company's on-chain name"}
-          </>
-        }
-      />
-    </Box>
-    <Box mt="32px">
-      <TextField
-        fullWidth
-        type="text"
-        label="Token Symbol"
-        error={!!props?.formState?.errors.symbol}
-        placeholder="ex: SPR"
-        {...props?.register('symbol', { required: true })}
-        helperText={
-          <>
-            {props?.formState?.errors.symbol && props?.formState?.errors.symbol.type === 'required'
-              ? 'You need to pick a token symbol in order to continue.'
-              : 'This will be used as your token symbol.'}
-          </>
-        }
-      />
-    </Box>
-  </>
-)
+const Name: React.FC<any> = (props) => {
+  const { chainId } = useParams()
+
+  const { data: names, isLoading, isSuccess } = useGetDaoNames(Number(chainId))
+
+  console.debug({ names, isLoading, isSuccess })
+
+  function isUniqueDaoName(candidateDaoName: string) {
+    return !names?.includes(candidateDaoName)
+  }
+
+  return (
+    <>
+      <Header title="Name" subtitle="Tell us the name of your company." />
+      <Box>
+        <TextField
+          fullWidth
+          type="text"
+          label="On-Chain name"
+          error={!!props?.formState?.errors.name}
+          placeholder="Pick a name"
+          {...props?.register('name', {
+            required: true,
+            validate: {
+              uniqueName: (v: string) => isUniqueDaoName(v) || 'This name is already used. Try another name.'
+            }
+          })}
+          helperText="This will be your company's on-chain name"
+        />
+        <ErrorMessage as={<Alert severity="error" />} errors={props?.formState?.errors} name="name" />
+      </Box>
+      <Box mt="32px">
+        <TextField
+          fullWidth
+          type="text"
+          label="Token Symbol"
+          error={!!props?.formState?.errors.symbol}
+          placeholder="ex: SPR"
+          {...props?.register('symbol', { required: true })}
+          helperText={
+            <>
+              {props?.formState?.errors.symbol && props?.formState?.errors.symbol.type === 'required'
+                ? 'You need to pick a token symbol in order to continue.'
+                : 'This will be used as your token symbol.'}
+            </>
+          }
+        />
+      </Box>
+    </>
+  )
+}
 
 export default Name
