b79ab7ba6a5e8defc8fef9d60ee12f716904d692
ivelin <ivelin.eth@gmail.com>
2022-12-08 15:57:37 +0000

    test: fix failing tests
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- frontend/src/layout/Page/index.tsx
+++ frontend/src/layout/Page/index.tsx
@@ -136,12 +136,14 @@ export function PageLayout(props: PageLayoutProps) {
           ) : (
             <></>
           )}
-          <Avatar alt="Sporos DAO logo" src="/logo192.png" sx={{ width: '32px', height: '32px' }} />
-          {!isMobile && (
+          <Link href="https://sporosdao.xyz/" rel="noopener" target="_blank">
+            <Avatar alt="Sporos DAO logo" src="/logo192.png" sx={{ width: '32px', height: '32px' }} />
+          </Link>
+          {/* {!isMobile && (
             <Typography variant="subtitle1" fontWeight={600} noWrap sx={{ ml: 2 }}>
               Sporos DAO - The Launchpad of For-Profit DAOs
             </Typography>
-          )}
+          )} */}
           <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1, ml: 2 }}>
             {isMyDaoLoaded && myDao?.token && `${myDao?.token?.name} (${myDao?.token?.symbol})`}
           </Typography>
