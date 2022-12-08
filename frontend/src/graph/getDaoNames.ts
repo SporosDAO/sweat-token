9c7169b44509927949cf8a23ce43456c8db3785b
ivelin <ivelin.eth@gmail.com>
2022-12-08 16:32:08 +0000

    test: 80% coverage for getDaoNames.ts
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- frontend/src/graph/getDaoNames.ts
+++ frontend/src/graph/getDaoNames.ts
@@ -2,26 +2,22 @@ import { useQuery } from 'react-query'
 import { GRAPH_URL } from './url'
 
 export async function getDaoNames(chainId: number) {
-  try {
-    const res = await fetch(GRAPH_URL[chainId], {
-      method: 'POST',
-      body: JSON.stringify({
-        query: `query {
+  const res = await fetch(GRAPH_URL[chainId], {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `query {
             daos(first: 1000) {
               token {
                 name
               }
             }
           }`
-      })
     })
-    const data = await res.json()
-    const names = data?.data?.daos?.map((dao: any) => dao.token?.name)
-    // console.debug({ data, names })
-    return names
-  } catch (e) {
-    return e
-  }
+  })
+  const data = await res.json()
+  const names = data?.data?.daos?.map((dao: any) => dao.token?.name)
+  // console.debug({ data, names })
+  return names
 }
 
 export function useGetDaoNames(chainId: number) {
